936bbdb768d63f7d8d80fd7f98550b9badee7cc4
niculaec <niculaec.alexandru@gmail.com>
2022-01-21 11:05:04 +0000

    Created AddNewContact UI
    AddNewContact activity

---
+++ app/src/main/java/com/appfactory/privacycontacts/AddNewContact.java
@@ -0,0 +1,13 @@
+package com.appfactory.privacycontacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_contact);
+    }
+}
