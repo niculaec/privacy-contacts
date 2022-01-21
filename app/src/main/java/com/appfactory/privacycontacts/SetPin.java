c24626f731304610a8c8e799e53ad7d959845a23
niculaec <niculaec.alexandru@gmail.com>
2022-01-21 11:02:37 +0000

    Created SetPin activity UI
    Was added SetPin activity

---
+++ app/src/main/java/com/appfactory/privacycontacts/SetPin.java
@@ -0,0 +1,14 @@
+package com.appfactory.privacycontacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetPin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_pin);
+    }
+}
