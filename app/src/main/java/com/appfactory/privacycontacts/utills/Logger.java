3cacdd6e7439b51569558ee17eac8db271bef237
niculaec <niculaec.alexandru@gmail.com>
2022-03-15 15:34:09 +0000

    Added Logger and PhoneInteractor.class

---
+++ app/src/main/java/com/appfactory/privacycontacts/utills/Logger.java
@@ -0,0 +1,9 @@
+package com.appfactory.privacycontacts.utills;
+
+import android.util.Log;
+
+public class Logger {
+    public static void log(String string) {
+        Log.d("contactsApp",string);
+    }
+}
---
+++ app/src/main/java/com/appfactory/privacycontacts/utills/PhoneInteractor.java
@@ -0,0 +1,17 @@
+package com.appfactory.privacycontacts.utills;
+
+public class PhoneInteractor {
+
+    void makeCall(String phoneNumber) {
+        Logger.log("The call is made");
+    }
+
+    void sendMessage(String phoneNumber) {
+        Logger.log("Message has been sent");
+    }
+
+    void SendEmail(String emailAddress) {
+        Logger.log("Email has been sent");
+
+    }
+}
