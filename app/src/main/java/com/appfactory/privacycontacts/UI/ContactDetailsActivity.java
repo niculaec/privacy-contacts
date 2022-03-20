9c0464e1493e12017887a0656063d73d9e5e5822
niculaec <niculaec.alexandru@gmail.com>
2022-03-20 18:02:59 +0000

    UI update ContactDetails.class
    Implementation of getText() of name,phone number and email address from EditText.

--- app/src/main/java/com/appfactory/privacycontacts/UI/ContactDetailsActivity.java
+++ app/src/main/java/com/appfactory/privacycontacts/UI/ContactDetailsActivity.java
@@ -3,8 +3,6 @@ package com.appfactory.privacycontacts.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.appfactory.privacycontacts.R;
 
@@ -14,11 +12,5 @@ public class ContactDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_details);
-        final EditText textName = findViewById(R.id.editTextPersonName);
-        final EditText phoneNumber = findViewById(R.id.editTextPhone);
-        final EditText emailAddress = findViewById(R.id.editTextEmailAddress);
-        textName.getText();
-        phoneNumber.getText();
-        emailAddress.getText();
     }
 }
