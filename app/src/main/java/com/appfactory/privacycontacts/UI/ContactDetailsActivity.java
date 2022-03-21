a7f1de70318ff661216c7a016f9fe073004d0981
niculaec <niculaec.alexandru@gmail.com>
2022-03-21 20:34:04 +0000

    Update ContactDetailsActivity
    was cerated setOnClickListener for iconCall, iconMessage, iconEmail.

--- app/src/main/java/com/appfactory/privacycontacts/UI/ContactDetailsActivity.java
+++ app/src/main/java/com/appfactory/privacycontacts/UI/ContactDetailsActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.appfactory.privacycontacts.R;
+import com.appfactory.privacycontacts.contact.Contact;
 
 public class ContactDetailsActivity extends AppCompatActivity {
 
@@ -37,5 +40,24 @@ public class ContactDetailsActivity extends AppCompatActivity {
         textNameEditText.getText();
         phoneNumberEditText.getText();
         emailAddressEditText.getText();
+        
+        iconCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ContactDetailsActivity.this, "Icon clickable test.", Toast.LENGTH_LONG).show();
+            }
+        });
+        iconMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ContactDetailsActivity.this, "Icon clickable test.", Toast.LENGTH_LONG).show();
+            }
+        });
+        iconEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ContactDetailsActivity.this, "Icon clickable test.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
