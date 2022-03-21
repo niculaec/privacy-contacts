1005062c8cf123fd2abb104129fc9e601dfdf123
niculaec <niculaec.alexandru@gmail.com>
2022-03-21 18:43:56 +0000

    update PhoneInteractor.java
    Tring to define the makeCall()

--- app/src/main/java/com/appfactory/privacycontacts/utills/PhoneInteractor.java
+++ app/src/main/java/com/appfactory/privacycontacts/utills/PhoneInteractor.java
@@ -1,8 +1,25 @@
 package com.appfactory.privacycontacts.utills;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.telephony.PhoneNumberUtils;
+import android.view.View;
+
+import com.appfactory.privacycontacts.UI.ContactDetailsActivity;
+
 public class PhoneInteractor {
+    ContactDetailsActivity contactDetailsActivity = new ContactDetailsActivity();
 
     void makeCall(String phoneNumber) {
+
+        iconCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse(phoneNumber));
+                contactDetailsActivity.startActivity(callIntent);
+            }
+        });
         Logger.log("The call is made");
     }
 
