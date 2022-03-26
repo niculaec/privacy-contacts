f0f7ee7bcb00be342a76fdc22e39d263ab6ee3fa
niculaec <niculaec.alexandru@gmail.com>
2022-03-26 10:52:18 +0000

    Updataed PhoneIOnteractor and PermitionManager class

---
+++ app/src/main/java/com/appfactory/privacycontacts/utills/PermissionsManager.java
@@ -0,0 +1,39 @@
+package com.appfactory.privacycontacts.utills;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class PermissionsManager {
+    Context context;
+
+    public boolean makeCallPermission() {
+        //request permission.
+        return ActivityCompat.checkSelfPermission(context,
+                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+//    public void textPermission(){
+//        if (ContextCompat.checkSelfPermission(
+//                CONTEXT, Manifest.permission.REQUESTED_PERMISSION) ==
+//                PackageManager.PERMISSION_GRANTED) {
+//            // You can use the API that requires the permission.
+//            performAction(...);
+//        } else if (shouldShowRequestPermissionRationale(...)) {
+//            // In an educational UI, explain to the user why your app requires this
+//            // permission for a specific feature to behave as expected. In this UI,
+//            // include a "cancel" or "no thanks" button that allows the user to
+//            // continue using your app without granting the permission.
+//            showInContextUI(...);
+//        } else {
+//            // You can directly ask for the permission.
+//            // The registered ActivityResultCallback gets the result of this request.
+//            requestPermissionLauncher.launch(
+//                    Manifest.permission.REQUESTED_PERMISSION);
+//        }
+//    }
+}
--- app/src/main/java/com/appfactory/privacycontacts/utills/PhoneInteractor.java
+++ app/src/main/java/com/appfactory/privacycontacts/utills/PhoneInteractor.java
@@ -1,27 +1,45 @@
 package com.appfactory.privacycontacts.utills;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.telephony.PhoneNumberUtils;
-import android.view.View;
-
-import com.appfactory.privacycontacts.UI.ContactDetailsActivity;
 
 public class PhoneInteractor {
-    ContactDetailsActivity contactDetailsActivity = new ContactDetailsActivity();
+    PermissionsManager permissionsManager = new PermissionsManager();
+    Context context;
 
-    void makeCall(String phoneNumber) {
+    public PhoneInteractor(Context context) {
+        this.context = context;
+    }
+
+
+    public void makeCall(String phoneNumber) {
         Intent callIntent = new Intent(Intent.ACTION_CALL);
-        callIntent.setData(Uri.parse(phoneNumber));
-        contactDetailsActivity.startActivity(callIntent);
+        // on below line we are setting data to it.
+        callIntent.setData(Uri.parse("tel:" + phoneNumber));
+        // on below line we are checking if the calling permissions are granted not.
+        if (permissionsManager.makeCallPermission()) {//request permission.
+            return;
+        }
+        context.startActivity(callIntent);
         Logger.log("The call is made");
     }
 
     void sendMessage(String phoneNumber) {
+        // on below line we are passing our contact number.
+        Intent smsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber));
+        smsIntent.putExtra("sms_body", "Enter your messaage");
+        context.startActivity(smsIntent);
         Logger.log("Message has been sent");
     }
 
     void SendEmail(String emailAddress) {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, emailAddress);
+        if (emailIntent.resolveActivity(this.context.getPackageManager()) != null) {
+            context.startActivity(emailIntent);
+        }
         Logger.log("Email has been sent");
 
     }
