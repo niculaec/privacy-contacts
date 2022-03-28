44aee7b8ff822113619dce72740d73077d6cf3d3
niculaec <niculaec.alexandru@gmail.com>
2022-03-28 11:17:41 +0100

    New hasPermission()
    Implementation of hasPermission() in: hasCallPermission(), hasMessagePermission(), public boolean hasEmailPermission().

--- app/src/main/java/com/appfactory/privacycontacts/utills/PermissionsManager.java
+++ app/src/main/java/com/appfactory/privacycontacts/utills/PermissionsManager.java
@@ -1,53 +1,41 @@
 package com.appfactory.privacycontacts.utills;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 public class PermissionsManager {
-    Context context;
+    Activity activity;
 
-    public boolean makeCallPermission() {
-        //request permission.
-        return ActivityCompat.checkSelfPermission(context,
-                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED;
+    public PermissionsManager(Activity activity) {
+        this.activity = activity;
     }
 
-    public boolean sendMessagePermission() {
-        //request permission.
-        return ActivityCompat.checkSelfPermission(context,
-                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED;
+    public boolean hasPermission(String permissionName) {
+        if(ActivityCompat.checkSelfPermission(activity,
+                permissionName) == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        ActivityCompat.requestPermissions(activity,new String[]{permissionName},0);
+        return false;
     }
 
-    public boolean sendEmailPermission() {
-        //request permission.
-        return ActivityCompat.checkSelfPermission(context,
-                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED;
+    public boolean hasCallPermission() {
+        return hasPermission(Manifest.permission.CALL_PHONE);
+        //Request permission to make a call
     }
 
+    public boolean hasMessagePermission() {
+        return  hasPermission(Manifest.permission.SEND_SMS);
+        //Request permission to send SMS.
+    }
 
+    public boolean hasEmailPermission() {
+        //TODO: Fix the email syntax.
+        return hasPermission(Manifest.permission.ADD_VOICEMAIL);
+        // Request permission to send email
+    }
 
-//    public void textPermission(){
-//        if (ContextCompat.checkSelfPermission(
-//                CONTEXT, Manifest.permission.REQUESTED_PERMISSION) ==
-//                PackageManager.PERMISSION_GRANTED) {
-//            // You can use the API that requires the permission.
-//            performAction(...);
-//        } else if (shouldShowRequestPermissionRationale(...)) {
-//            // In an educational UI, explain to the user why your app requires this
-//            // permission for a specific feature to behave as expected. In this UI,
-//            // include a "cancel" or "no thanks" button that allows the user to
-//            // continue using your app without granting the permission.
-//            showInContextUI(...);
-//        } else {
-//            // You can directly ask for the permission.
-//            // The registered ActivityResultCallback gets the result of this request.
-//            requestPermissionLauncher.launch(
-//                    Manifest.permission.REQUESTED_PERMISSION);
-//        }
-//    }
 }
