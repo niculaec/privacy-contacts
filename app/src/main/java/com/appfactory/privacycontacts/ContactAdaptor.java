f5239d5eaabdd2399785cbff24e7f63c61b8e09e
niculaec <niculaec.alexandru@gmail.com>
2022-03-28 16:10:30 +0100

    New ContactAdaptor.java
    this class define an adaptor for ContactList.java

---
+++ app/src/main/java/com/appfactory/privacycontacts/ContactAdaptor.java
@@ -0,0 +1,47 @@
+package com.appfactory.privacycontacts;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.appfactory.privacycontacts.contact.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactAdaptor extends ArrayAdapter<Contact> {
+
+    ArrayList<Contact> mContacts;
+    LayoutInflater mInflater;
+
+    public ContactAdaptor(@NonNull Context context,  @NonNull List<Contact> objects) {
+        super(context, R.layout.contacts_lw_item, objects);
+
+        mContacts= (ArrayList<Contact>) objects;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.contacts_lw_item, parent,false);
+        }
+        TextView textViewName = convertView.findViewById(R.id.personNameTextView);
+        ImageView imageViewUserPicture = convertView.findViewById(R.id.userPicture);
+
+        Contact contact = mContacts.get(position);
+        textViewName.setText(contact.getName());
+        imageViewUserPicture.setImageResource(R.drawable.logo);
+
+        return super.getView(position, convertView, parent);
+    }
+}
