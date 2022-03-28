1b57bc6083d25c7820bce2e254c725e2f27605e5
niculaec <niculaec.alexandru@gmail.com>
2022-03-28 11:14:22 +0100

    Updated PhoneInteractor.java
    This Class is now compleated

--- app/src/main/java/com/appfactory/privacycontacts/utills/PhoneInteractor.java
+++ app/src/main/java/com/appfactory/privacycontacts/utills/PhoneInteractor.java
@@ -1,45 +1,48 @@
 package com.appfactory.privacycontacts.utills;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
 public class PhoneInteractor {
-    PermissionsManager permissionsManager = new PermissionsManager();
+    PermissionsManager permissionsManager;
     Context context;
 
-    public PhoneInteractor(Context context) {
-        this.context = context;
+    public PhoneInteractor(Activity activity) {
+        permissionsManager = new PermissionsManager(activity);
+        this.context = activity;
     }
 
     public void makeCall(String phoneNumber) {
+        if (!permissionsManager.hasCallPermission()) {
+            return;
+        }
         Intent callIntent = new Intent(Intent.ACTION_CALL);
         // on below line we are setting data to it.
         callIntent.setData(Uri.parse("tel:" + phoneNumber));
         // on below line we are checking if the calling permissions are granted not.
-        if (permissionsManager.makeCallPermission()) {//request permission.
-            return;
-        }
         context.startActivity(callIntent);
         Logger.log("The call is made");
     }
 
     public void sendMessage(String phoneNumber) {
         // on below line we are passing our contact number.
+        if (!permissionsManager.hasMessagePermission()) {
+            return;
+        }
         Intent smsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber));
-        smsIntent.putExtra("sms_body", "Enter your message");
         context.startActivity(smsIntent);
         Logger.log("Message has been sent");
     }
 
     public void sendEmail(String emailAddress) {
+        if (!permissionsManager.hasEmailPermission()) {
+            return;
+        }
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
         emailIntent.setData(Uri.parse("mailto:" + emailAddress));
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, emailAddress);
-        if (permissionsManager.sendEmailPermission()) {
-            context.startActivity(emailIntent);
-        }
+        context.startActivity(emailIntent);
         Logger.log("Email has been sent");
-
     }
 }
