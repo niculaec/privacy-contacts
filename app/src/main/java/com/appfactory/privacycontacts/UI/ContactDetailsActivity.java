30bde29bd8116cddd2b814341746bcd872a167b6
niculaec <niculaec.alexandru@gmail.com>
2022-03-29 13:12:50 +0100

    updated ContactDetailsActivity
    was modified after one example

--- app/src/main/java/com/appfactory/privacycontacts/UI/ContactDetailsActivity.java
+++ app/src/main/java/com/appfactory/privacycontacts/UI/ContactDetailsActivity.java
@@ -2,6 +2,7 @@ package com.appfactory.privacycontacts.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +28,11 @@ public class ContactDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_details);
+
+        getSelectedContact();
+        setValues();
+
+        contact = ContactsListActivity.contactsList.get(0);
 
         personNameTextView = findViewById(R.id.personNameTextView);
         phoneNumberTextView = findViewById(R.id.phoneTextView);
@@ -83,5 +89,19 @@ public class ContactDetailsActivity extends AppCompatActivity {
         phoneNumberTextView.setText(contact.getPhoneNumber());
         emailAddressTextView.setText(contact.getEmailAddress());
         //userPicture.setImageURI(contact.getUserPicture());
+    }
+
+    private void getSelectedContact() {
+        Intent previousIntent = getIntent();
+        String parsedStringName = previousIntent.getStringExtra("name");
+        contact = ContactsManager.contactsList.get(Integer.parseInt(parsedStringName));
+        //MainActivity.shapeList.get(Integer.valueOf(parsedStringID))
+    }
+
+    private void setValues() {
+        TextView textViewName = findViewById(R.id.personNameTextView);
+
+        personNameTextView.setText(contact.getName());
+        userPicture.setImageResource(Integer.parseInt(contact.getUserPicture()));
     }
 }
