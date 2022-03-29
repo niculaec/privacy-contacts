777d5f33d882135242bed722b25960c171a05d25
niculaec <niculaec.alexandru@gmail.com>
2022-03-29 13:10:31 +0100

    update ContactAdapter
    was modified after une example

--- app/src/main/java/com/appfactory/privacycontacts/ContactAdapter.java
+++ app/src/main/java/com/appfactory/privacycontacts/ContactAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,31 +13,27 @@ import androidx.annotation.Nullable;
 
 import com.appfactory.privacycontacts.contact.Contact;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ContactAdapter extends ArrayAdapter<Contact> {
 
-    ArrayList<Contact> mContacts;
-    LayoutInflater mInflater;
 
-    public ContactAdapter(@NonNull Context context, @NonNull List<Contact> objects) {
-        super(context, R.layout.contacts_lw_item, objects);
-
-        mContacts= (ArrayList<Contact>) objects;
-        mInflater = LayoutInflater.from(context);
+    public ContactAdapter(@NonNull Context context, int resource, @NonNull List<Contact> contactList) {
+        super(context, resource, contactList);
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        if (convertView == null) {
-            convertView = mInflater.inflate(R.layout.contacts_lw_item, parent,false);
-        }
-        TextView textViewName = convertView.findViewById(R.id.personNameTextView);
-        ImageView imageViewUserPicture = convertView.findViewById(R.id.userPicture);
+        Contact contact = getItem(position);
 
-        Contact contact = mContacts.get(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.contact_single_item, parent, false);
+        }
+        TextView textViewName =(TextView) convertView.findViewById(R.id.personNameTextView);
+        ImageView imageViewUserPicture = (ImageView) convertView.findViewById(R.id.userPicture);
+
+        //Contact contact = mContacts.get(position);
         textViewName.setText(contact.getName());
         imageViewUserPicture.setImageResource(R.drawable.logo);
 
