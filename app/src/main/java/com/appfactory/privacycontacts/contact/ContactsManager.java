45e46d789fe4d63023c6260d2f34a560f684e780
niculaec <niculaec.alexandru@gmail.com>
2022-04-10 12:32:19 +0100

    Update ContactManger.java
    was created a instance arrayAdapter of ArrayAdapter;
    new method notifyAdapter to notify the adapter for any changes and calling this method in every method witch can produce changes of contactsList;
    method setArrayAdapter assigns the class adapter to the method adapter;

--- app/src/main/java/com/appfactory/privacycontacts/contact/ContactsManager.java
+++ app/src/main/java/com/appfactory/privacycontacts/contact/ContactsManager.java
@@ -1,5 +1,7 @@
 package com.appfactory.privacycontacts.contact;
 
+import android.widget.ArrayAdapter;
+
 import java.util.ArrayList;
 
 import com.appfactory.privacycontacts.R;
@@ -11,6 +13,7 @@ public class ContactsManager {
     private final ArrayList<Contact> contactsList = new ArrayList<Contact>();
     private final ContactRepository contactRepository = new ContactRepository();
     public static final String ID_KEY = "id";
+    private ArrayAdapter<Contact> arrayAdapter;
 
 
     private ContactsManager() {
@@ -29,7 +32,18 @@ public class ContactsManager {
     public void addContact(Contact aContact) {
         contactsList.add(aContact);
         contactRepository.saveContact(aContact);
+        notifyAdapter();
         Logger.log("The contact " + aContact + " was successfully added to the contacts list.");
+    }
+
+    public void notifyAdapter(){
+        if(arrayAdapter != null){
+            arrayAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public void setArrayAdapter(ArrayAdapter arrayAdapter){
+        this.arrayAdapter = arrayAdapter;
     }
 
     public Contact updateContact(Contact aContact, String name, String phoneNUmber, String emailAddress, String userPicture) {
@@ -43,6 +57,7 @@ public class ContactsManager {
         contactsList.add(contactsList.indexOf(aContact), newContact);
         contactsList.remove(aContact);
         contactRepository.updateContact(newContact);
+        notifyAdapter();// notify the adaptor for changes.
         Logger.log("UpdateContact \n New details: " + newContact + "\n Old details: " + aContact);
         return newContact;
     }
@@ -50,6 +65,7 @@ public class ContactsManager {
     public void removeContact(Contact aContact) {
         contactsList.remove(aContact);
         contactRepository.removeContact(aContact);
+        notifyAdapter();
         Logger.log(this + " removeContact\n The " + aContact + " \n Was successfully removed from the contacts list.");
     }
 
