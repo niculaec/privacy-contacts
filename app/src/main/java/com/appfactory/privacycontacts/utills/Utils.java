80f058cc7bc494a50fbba054a868fcd1060313db
niculaec <niculaec.alexandru@gmail.com>
2022-04-15 10:42:53 +0100

    Update Utils, was added new code comments;

--- app/src/main/java/com/appfactory/privacycontacts/utills/Utils.java
+++ app/src/main/java/com/appfactory/privacycontacts/utills/Utils.java
@@ -14,21 +14,18 @@ public class Utils {
 
     public static String getBase64FromUri(Uri uri) {
         try {
-            Bitmap bitmap = MediaStore.Images.Media.getBitmap(AddNewContactActivity.this.getContentResolver(), uri);
-
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(AddNewContactActivity.this.getContentResolver(), uri);// getting the Bitmap from mediaStore
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();//creating an Array of bytes
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);// compress the data to PNG format and keep the 100% quality
             byte[] byteArray = outputStream.toByteArray();
-
-            //Use your Base64 String as you wish
-            return Base64.encodeToString(byteArray, Base64.DEFAULT);
+            return Base64.encodeToString(byteArray, Base64.DEFAULT);// encode the base64 to string
         }catch (Exception e){
-            return "";
+            return "";// return an empty string
         }
     }
 
-    public static Bitmap getBitmapFromBase64(String base64){
+    public static Bitmap getBitmapFromBase64(String base64){// deserializer the user picture from bytes to string
         byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
-        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);// decode the byte array to string
     }
 }
