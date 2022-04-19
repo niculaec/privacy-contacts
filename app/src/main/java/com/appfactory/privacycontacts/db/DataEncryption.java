96b1fda9baa551bcbd24daa7e8277a7288dda6b9
niculaec <niculaec.alexandru@gmail.com>
2022-04-19 19:28:00 +0100

    Update DataEncryption new methods to encrtpt and decrypt data

--- app/src/main/java/com/appfactory/privacycontacts/db/DataEncryption.java
+++ app/src/main/java/com/appfactory/privacycontacts/db/DataEncryption.java
@@ -1,15 +1,33 @@
 package com.appfactory.privacycontacts.db;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class DataEncryption {
+    private static final DataEncryption INSTANCE = new DataEncryption();
+    private static String password;
 
     private DataEncryption() {
     }
 
-    public static String getMD5EncryptedString(String encTarget) {
+    public static DataEncryption getInstance(){
+        return INSTANCE;}
+
+
+    public String getMD5EncryptedString(String encTarget) {
         MessageDigest mdEnc = null;
         try {
             mdEnc = MessageDigest.getInstance("MD5");
@@ -23,5 +41,33 @@ public class DataEncryption {
             md5 = "0" + md5;
         }
         return md5;
+    }
+
+    public SecretKey generateKey(SecretKeySpec secret)
+            throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        return secret = new SecretKeySpec ( password.getBytes(), "AES");
+    }
+
+    public byte[] encryptMsg(String message, SecretKey secret)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidParameterSpecException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException
+    {
+        /* Encrypt the message. */
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        byte[] cipherText = cipher.doFinal(message.getBytes("UTF-8"));
+        return cipherText;
+    }
+
+    public String decryptMsg(byte[] cipherText, SecretKey secret)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidParameterSpecException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException
+    {
+        /* Decrypt the message, given derived encContentValues and initialization vector. */
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secret);
+        String decryptString = new String(cipher.doFinal(cipherText), "UTF-8");
+        return decryptString;
     }
 }
