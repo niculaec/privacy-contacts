bfaa0cd16321ec7762c125eac27a807cc7972a4f
niculaec <niculaec.alexandru@gmail.com>
2022-04-23 22:17:14 +0100

    Updated ContactsListTest.java added new import
    new import

--- app/src/androidTest/java/com/appfactory/privacycontacts/UI/ContactsListTest.java
+++ app/src/androidTest/java/com/appfactory/privacycontacts/UI/ContactsListTest.java
@@ -4,6 +4,7 @@ import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
