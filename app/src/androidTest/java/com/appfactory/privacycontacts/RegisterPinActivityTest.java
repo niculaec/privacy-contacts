3dd144c6320ba95040e12b6f488b670e9d775cdc
niculaec <niculaec.alexandru@gmail.com>
2022-04-23 19:12:23 +0100

    Change class name.

--- app/src/androidTest/java/com/appfactory/privacycontacts/RegisterPinActivityTest.java
+++ app/src/androidTest/java/com/appfactory/privacycontacts/RegisterPinActivityTest.java
@@ -1,18 +1,17 @@
 package com.appfactory.privacycontacts;
 
-import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-import android.content.Context;
-import android.provider.ContactsContract;
 
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.ViewAction;
+
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -21,13 +20,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
 
+import android.content.Intent;
 
-import com.appfactory.privacycontacts.UI.ContactsListActivity;
-import com.appfactory.privacycontacts.UI.EnterPinActivity;
 import com.appfactory.privacycontacts.UI.LandingActivity;
-import com.appfactory.privacycontacts.UI.SetPinActivity;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -35,7 +32,7 @@ import com.appfactory.privacycontacts.UI.SetPinActivity;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class RegisterPinActivityTest {
 
     @Rule
     public ActivityScenarioRule<LandingActivity> activityRule =
@@ -43,6 +40,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void pinEditTextHintTest() {
         onView(withId(R.id.textEnterPin)).check(matches(withHint("Please enter your PIN")));
+        onView(withId(R.id.textConfirmPin)).check(matches(withHint("Confirm PIN")));
     }
 
     @Test
@@ -50,7 +48,7 @@ public class ExampleInstrumentedTest {
         onView(withId(R.id.textEnterPin)).perform(ViewActions.typeText("1234"),ViewActions.closeSoftKeyboard());
         onView(withId(R.id.textConfirmPin)).perform(ViewActions.typeText("1234"),ViewActions.closeSoftKeyboard());
         onView(withId(R.id.buttonSetPinOk)).perform(click());
-
+        restart();
         clearStorageAppDetails();// at the end of the test
 
     }
@@ -58,13 +56,12 @@ public class ExampleInstrumentedTest {
     public void clearStorageAppDetails(){
         InstrumentationRegistry.getInstrumentation().getTargetContext().deleteDatabase("contacts-database");
         InstrumentationRegistry.getInstrumentation().getTargetContext().deleteSharedPreferences("com.appfactory.privacycontacts_preferences");
-        //InstrumentationRegistry.getInstrumentation().getTargetContext().
     }
 
-    public void resetApp(){
-            ContactsContract.Intents.release()
-            rule.scenario.close()
-            ContactsContract.Intents.init()
-            ActivityScenario.launch<LandingActivity>(intent)
+    private void restart() {
+        Intents.release();
+        activityRule.getScenario().close();
+        Intents.init();
+        ActivityScenario.launch(new Intent(InstrumentationRegistry.getInstrumentation().getTargetContext(), LandingActivity.class));
     }
 }
