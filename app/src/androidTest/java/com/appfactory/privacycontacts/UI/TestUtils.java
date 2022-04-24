bb6faf0ea54d0fb6f08a0c8822de621553878fc9
niculaec <niculaec.alexandru@gmail.com>
2022-04-24 02:18:06 +0100

    Updated TestUtils.java
    New method was created allowPermissionsIfNeeded() to allow permitions.

--- app/src/androidTest/java/com/appfactory/privacycontacts/UI/TestUtils.java
+++ app/src/androidTest/java/com/appfactory/privacycontacts/UI/TestUtils.java
@@ -3,24 +3,29 @@ package com.appfactory.privacycontacts.UI;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
 
 import com.appfactory.privacycontacts.R;
 
 public class TestUtils {
 
-    public static void clearStorageAppDetails(){
-        InstrumentationRegistry.getInstrumentation().getTargetContext().deleteDatabase("contacts-database");
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(    InstrumentationRegistry.getInstrumentation().getTargetContext());
+    public static void clearStorageAppDetails() {
+        getInstrumentation().getTargetContext().deleteDatabase("contacts-database");
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getInstrumentation().getTargetContext());
         SharedPreferences.Editor editor = sharedPrefs.edit();
         editor.clear();
         editor.commit();
@@ -33,17 +38,31 @@ public class TestUtils {
         Intents.release();
         activityRule.getScenario().close();
         Intents.init();
-        ActivityScenario.launch(new Intent(InstrumentationRegistry.getInstrumentation().getTargetContext(), LandingActivity.class));
+        ActivityScenario.launch(new Intent(getInstrumentation().getTargetContext(), LandingActivity.class));
     }
 
-    public static void registerWithPin(){
-        onView(withId(R.id.textEnterPin)).perform(ViewActions.typeText("1234"),ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.textConfirmPin)).perform(ViewActions.typeText("1234"),ViewActions.closeSoftKeyboard());
+    public static void registerWithPin() {
+        onView(withId(R.id.textEnterPin)).perform(ViewActions.typeText("1234"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.textConfirmPin)).perform(ViewActions.typeText("1234"), ViewActions.closeSoftKeyboard());
         onView(withId(R.id.buttonSetPinOk)).perform(click());
     }
 
-    public static void loginWithPin(){
-        onView(withId(R.id.textEnterPin)).perform(ViewActions.typeText("1234"),ViewActions.closeSoftKeyboard());// testing EnterPinActivity
+    public static void loginWithPin() {
+        onView(withId(R.id.textEnterPin)).perform(ViewActions.typeText("1234"), ViewActions.closeSoftKeyboard());// testing EnterPinActivity
         onView(withId(R.id.buttonLoginOk)).perform(click());
+    }
+
+    public static void allowPermissionsIfNeeded() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            UiDevice device = UiDevice.getInstance(getInstrumentation());
+            UiObject allowPermissions = device.findObject(new UiSelector().text("ALLOW"));
+            if (allowPermissions.exists()) {
+                try {
+                    allowPermissions.click();
+                } catch (UiObjectNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
