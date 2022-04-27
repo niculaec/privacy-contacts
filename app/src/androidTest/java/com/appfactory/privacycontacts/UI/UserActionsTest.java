6984529a93648ae5df49d210cbaa24fe83484230
niculaec <niculaec.alexandru@gmail.com>
2022-04-27 21:59:07 +0100

    Updated UserActionTest last version

--- app/src/androidTest/java/com/appfactory/privacycontacts/UI/UserActionsTest.java
+++ app/src/androidTest/java/com/appfactory/privacycontacts/UI/UserActionsTest.java
@@ -2,13 +2,18 @@ package com.appfactory.privacycontacts.UI;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+import static androidx.test.espresso.intent.Intents.intended;
+
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+
+import android.content.Intent;
+
 import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -20,6 +25,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
+
 @RunWith(AndroidJUnit4.class)
 public class UserActionsTest {
     @Rule //the rule of test
@@ -27,30 +34,29 @@ public class UserActionsTest {
             new ActivityScenarioRule<>(LandingActivity.class);
     @Before // Clear device storage before test to start
     public void clearStorage() {
+        Intents.init();
         TestUtils.clearStorageAppDetails();
     }
 
     @After // Clear device storage after testing
     public void clearStorageAfter() {
+        Intents.release();
         TestUtils.clearStorageAppDetails();
     }
 
     @Test
     public void performCallTest() {
+
         TestUtils.registerWithPin();
         onView(withId(R.id.floatingActionButtonAdd)).perform(click());
         onView(withId(R.id.editTextPersonName)).perform(ViewActions.typeText("Alexandru"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.editTextPhone)).perform(ViewActions.typeText("077777777"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.editTextPhone)).perform(ViewActions.typeText("abd"), ViewActions.closeSoftKeyboard());
         onView(withId(R.id.editTextEmailAddress)).perform(ViewActions.typeText("Alexandru@gmail.com"), ViewActions.closeSoftKeyboard());
         onView(withId(R.id.buttonSave)).perform(click());
 
-        TestUtils.restartApplication(activityRule);
-
-        TestUtils.loginWithPin();
         onView(withText("Alexandru")).perform(ViewActions.click());
         onView(withId(R.id.iconCall)).perform(click());
-        TestUtils.allowPermissionsIfNeeded();
-        // onView(withId(R.id.iconCall)).perform(click());
+        intended(IntentMatchers.hasAction(Intent.ACTION_CALL));
 
     }
 }
